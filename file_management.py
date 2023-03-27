6019939686d206e160c44bbd64688ea56bb98e39
hetlesaeterta <hetlesaetherdev@gmail.com>
2023-03-27 18:59:48 +0200

    finished initialization functions

--- file_management.py
+++ file_management.py
@@ -1,23 +1,83 @@
 from pyicloud import PyiCloudService
+from shutil import copyfileobj
 from time import sleep
 import ping3
+import io
+import os
+import sys
 
-try:
-    ping.verbose_ping("www.apple.com", count=3)
-except:
-    print("Fail to ping apple.com, check if you're connected to internet")
+ping3.EXCEPTIONS = True
 
-# username = "hetlesaetherspam@gmail.com"
-# pw = open("password.txt", "r")
-# password = pw.read()
-# pw.close()
+#TODO add log files insted of printing minor errors
+class initalize:
+    def checkConnection():
+        try:
+            # ping3 will print text not return it. Disabling print to output is not visable to user.
+            text_trap = io.StringIO()
+            sys.stdout = text_trap
+            ping3.verbose_ping("www.apple.com")
+            sys.stdout = sys.__stdout__
+            
+            return ("Apple.com - OK", 0)
+        except:
+            return ("Fail to ping apple.com, check if you're connected to internet", 1)
 
-# api = PyiCloudService(username, password)
+    def checkSignIn():
+        try:
+            username = "hetlesaetherspam@gmail.com"
+            pw = open("password.txt", "r")
+            password = pw.read()
+            pw.close()
+            
+            global api
+            api = PyiCloudService(username, password)
 
-# api.params['clientID'] = api.client_id
-# api.drive.params["clientId"] = api.client_id
+            api.params['clientID'] = api.client_id
+            api.drive.params["clientId"] = api.client_id
+            return ("Sign In - OK", 0)
+        except:
+            return ("Failed to sign into your iCloud account, check if you're using valid credentials", 1)
+    
+    def checkDirectory(): 
+        if "TaskTrace" not in api.drive.dir():
+            response = api.drive.mkdir('TaskTrace')
+            if response['folders'][0]['status'] != "OK":
+                return ("Filed to initialize directory - Send in an issue on github", 1)
+        return ("Directory - OK", 0)
+    def checkReadWrite():
+        f = open("test.txt", 'w')
+        f.write("a")
+        f.close()
+        
+        try:
+            with open("test.txt", 'rb') as f:
+                api.drive['TaskTrace'].upload(f)
+        except:
+            return ("Failed to write to iCloud", 1)
 
-# if "TaskTrace" not in api.drive.dir():
-#     response = api.drive.mkdir('TaskTrace')
-#     if response['folders'][0]['status'] != "OK":
-#         raise Exception("Not able to create TaskTrace folder in root")
+        try:
+            testfile = api.drive['TaskTrace']['test.txt']  
+            with testfile.open(stream=True) as response:
+                with open("testreturn.txt", 'wb') as file_out:
+                    copyfileobj(response.raw, file_out)
+            if open("testreturn.txt", 'r').read() != 'a':
+                raise Exception("error")
+        except: 
+            return ("Failed to read from iCloud", 1)
+
+        try:
+            api.drive['TaskTrace']['test.txt'].delete()
+        except:
+            return ("Failed to delete from iCloud", 1)
+        
+        if os.path.exists("test.txt"):
+            os.remove("test.txt")
+        else:
+            print("Minor Warning - unable to find and delete local test file")
+        if os.path.exists("testreturn.txt"):
+            os.remove("testreturn.txt")
+        else:
+            print("Minor Warning - unable to find and delete local test file")
+        return ("Read/Write - OK", 0)
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+from file_management import initalize
+from colorama import init
+from termcolor import colored
+
+init()
+
+OFFLINE_MODE = False
+startup_functions = [initalize.checkConnection, initalize.checkSignIn, initalize.checkDirectory, initalize.checkReadWrite]
+startup_functions_return_value = ""
+
+for function in startup_functions:
+    startup_functions_return_value = function()
+    bg_color = "on_green"
+    if startup_functions_return_value[1] == 1:
+        OFFLINE_MODE = True
+        bg_color = "on_red"
+    print(colored(startup_functions_return_value[0], "white", bg_color))
